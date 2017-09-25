517b5fd6929206bafd0c0b8b372981dac0fcfefa
Jigar Mistry <jigarmistry24@gmail.com>
2017-09-25 14:10:38 +0530

    Initial App

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index");
+var users = require("./routes/users");
+
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.end();
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, OPTIONS");
+    next();
+  }
+};
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(allowCrossDomain);
+app.use("/", index);
+app.use("/users", users);
+
+// Socket Connection
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+
+app.post("/messages", function(req, res) {
+  io.send(req.body.message);
+  res.send("ok");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+io.on("connection", function() {
+  console.log("Connection");
+});
+server.listen(4567);
+
+module.exports = app;
